a2e46ba4fbbb30a559b3ea49e6ee5bf4eab3fd61
slouham <salmanelouham@gmail.com>
2024-02-09 21:13:16 +0100

    gnl

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: slouham <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 11:59:13 by slouham           #+#    #+#             */
-/*   Updated: 2024/02/09 19:11:41 by slouham          ###   ########.fr       */
+/*   Updated: 2024/02/09 21:10:34 by slouham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,9 @@
 # include <stdio.h>
 
 char	*get_next_line(int fd);
-char	*ft_strdup(char *s);
+char	*ft_free(char *buffer);
+char	*ft_strjoin(const char *s1,const char *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,13 +6,19 @@
 /*   By: slouham <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 11:59:31 by slouham           #+#    #+#             */
-/*   Updated: 2024/02/09 19:40:32 by slouham          ###   ########.fr       */
+/*   Updated: 2024/02/09 21:10:57 by slouham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_strlen(const char *s)
+char	*ft_free(char *buffer)
+{
+	free(buffer);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
@@ -38,22 +44,28 @@ char	*ft_strchr(const char *s, int c)
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len_s1;
-	size_t	len_s2;
-	char	*total;
+	int		sizetotal;
+	char	*res;
+	int		i;
+	int		j;
 
-	if (!s1 && !s2)
-		return (0);
-	if (!s1)
-		return (ft_strdup(s2));
-	if (!s2)
-		return (ft_strdup(s1));
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	total = (char *)malloc(len_s1 + len_s2 + 1);
-	if (!total)
+	i = 0;
+	sizetotal = ft_strlen(s1) + ft_strlen(s2);
+	res = malloc(sizeof(char) * (sizetotal + 1));
+	if (!res || !s1 || !s2)
 		return (NULL);
-	ft_strlcpy(total, s1, len_s1 + 1);
-	ft_strlcat(total + len_s1, s2, len_s2 + 1);
-	return (total);
+	while (s1[i] != 0)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != 0)
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[sizetotal] = 0;
+	return (res);
 }
